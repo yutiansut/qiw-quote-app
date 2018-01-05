1437e5eb02a07e209f02cd2f1537baddff9394aa
wuxuanfeng <527140631@qq.com>
2018-01-05 17:23:33 +0800

    111

---
+++ src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index">
+    <h1>{{ msg }}</h1>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1{
+  font-weight: normal;
+}
+</style>
