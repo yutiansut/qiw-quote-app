38d0e34aa26f5e0a7aa71e797cdf47b6912172d7
wuxuanfeng <527140631@qq.com>
2018-01-05 17:32:20 +0800

    111

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mint from 'mint-ui';
+Vue.use(Mint);
+import 'mint-ui/lib/style.css'; 
 
 Vue.config.productionTip = false
 
