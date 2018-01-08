418131032400fff7c076ba182fd9c6baa1ef7cbc
wuxuanfeng <527140631@qq.com>
2018-01-08 20:11:17 +0800

    111

--- src/page/index.vue
+++ src/page/index.vue
@@ -17,7 +17,69 @@
 			<span>债券期货</span>
 			<span>ETF</span>
 		</nav>
-		
+		<div class="recommend">
+			<div class="col">
+				<span class="name">美原油</span>
+				<div class="price">
+					<span class="red">1304.49</span>
+					<i class="icon icon_arrow up"></i></span>
+				</div>
+				<span class="red">+0.05%&nbsp;&nbsp;+0.69</span>
+			</div>
+			<div class="col">
+				<span class="name">美黄金</span>
+				<div class="price">
+					<span class="green">1304.49</span>
+					<i class="icon icon_arrow down"></i></span>
+				</div>
+				<span class="green">+0.05%&nbsp;&nbsp;+0.69</span>
+			</div>
+			<div class="col">
+				<span class="name">美铜</span>
+				<div class="price">
+					<span class="red">1304.49</span>
+					<i class="icon icon_arrow up"></i></span>
+				</div>
+				<span class="red">+0.05%&nbsp;&nbsp;+0.69</span>
+			</div>
+		</div>
+		<div class="list">
+			<ul>
+				<li>
+					<div class="name"><span>名称</span></div>
+					<span>价格</span>
+					<span>成交量</span>
+					<span>涨跌幅<i class="icon icon_switch"></i></span>
+				</li>
+				<li>
+					<div class="name">
+						<em>小原油</em>
+						<em>QM1801</em>
+					</div>
+					<span class="red">50.15</span>
+					<span>1500</span>
+					<span class="red">+0.03%</span>
+				</li>
+				<li>
+					<div class="name">
+						<em>小原油</em>
+						<em>QM1801</em>
+					</div>
+					<span class="green">50.15</span>
+					<span>1500</span>
+					<span class="green">+0.03%</span>
+				</li>
+				<li>
+					<div class="name">
+						<em>小原油</em>
+						<em>QM1801</em>
+					</div>
+					<span class="red">50.15</span>
+					<span>1500</span>
+					<span class="red">+0.03%</span>
+				</li>
+			</ul>
+		</div>
 		
 	</div>
 </template>
@@ -64,8 +126,10 @@
 				line-height: 0.6rem;
 				text-align: center;
 				background: $lightBlue;
+				font-size: $fs28;
 				&.current{
 					background: $black;
+					color: $white;
 				}
 			}
 		}
@@ -85,12 +149,144 @@
 		left: 0;
 		width: $w;
 		height: 0.8rem;
-		line-height: 0.8rem;
 		background: $bg;
 		border-bottom: 0.01rem solid $black;
-		
+		padding: 0 0.3rem;
+		span{
+			display: inline-block;
+			height: 0.75rem;
+			line-height: 0.75rem;
+			border-bottom: 0.04rem solid $bg;
+			font-size: $fs28;
+			margin-left: 0.3rem;
+			&:first-child{
+				margin: 0;
+			}
+			&.current{
+				color: $blue;
+				border-color: $blue;
+			}
+		}
 	}
-	
+	.recommend{
+		position: fixed;
+		top: 1.82rem;
+		left: 0;
+		width: 7.5rem;
+		height: 1.6rem;
+		border-bottom: 0.01rem solid $black;
+		.col{
+			float: left;
+			width: 2.5rem;
+			height: 1.6rem;
+			border-right: 0.01rem solid $black;
+			span{
+				display: block;
+				text-align: center;
+				font-size: $fs32;
+				&.name{
+					color: $white;
+					margin-top: 0.25rem;
+				}
+				&:last-child{
+					font-size: 0.2rem;
+				}
+				&.red{
+					color: $red;
+				}
+				&.green{
+					color: $green;
+				}
+			}
+			.price{
+				height: 0.6rem;
+				text-align: center;
+				span{
+					float: left;
+					display: inline-block;
+					line-height: 0.6rem;
+					margin-left: 0.45rem;
+				}
+				.icon_arrow{
+					float: left;
+					width: 0.2rem;
+					height: 0.36rem;
+					background-size: 100% 100%;
+					margin: 0.1rem 0 0 0.1rem;
+					&.up{
+						background: url(../assets/images/arrow_up.png) no-repeat center top;
+					}
+					&.down{
+						background: url(../assets/images/arrow_down.png) no-repeat center top;
+					}
+				}
+			}
+		}
+	}
+	.list{
+		width: 7.5rem;
+		margin-top: 3.42rem;
+		li{
+			height: 0.96rem;
+			line-height: 0.96rem;
+			border-bottom: 0.01rem solid $black;
+			padding: 0 0.3rem;
+			&:first-child{
+				height: 0.64rem;
+				line-height: 0.64rem;
+				span{
+					color: $fontBlue !important;
+				}
+			}
+			span{
+				float: left;
+				display: inline-block;
+				font-size: $fs28;
+				text-align: right;
+				&:nth-child(1){
+					width: 3rem;
+					text-align: left;
+				}
+				&:nth-child(2){
+					width: 1.25rem;
+				}
+				&:nth-child(3){
+					width: 1.25rem;
+					color: $white;
+				}
+				&:nth-child(4){
+					width: 1.4rem;
+				}
+				&.red{
+					color: $red;
+				}
+				&.green{
+					color: $green;
+				}
+				.icon_switch{
+					width: 0.2rem;
+					height: 0.24rem;
+					background: url(../assets/images/switch.png) no-repeat center center;
+					background-size: 0.18rem 0.24rem;
+					margin-left: 0.05rem;
+					margin-top: 0.1rem;
+				}
+			}
+			.name{
+				float: left;
+				width: 3rem;
+				em{
+					display: block;
+					line-height: 0.36rem;
+					&:first-child{
+						margin-top: 0.1rem;
+						font-size: $fs32;
+						color: $white;
+					}
+				}
+			}
+		}
+	}
 	
 	
 </style>
