a61834e7186d1b793466299675b0f547d5bf51f6
wuxuanfeng <527140631@qq.com>
2018-01-08 14:33:15 +0800

    111

--- src/page/index.vue
+++ src/page/index.vue
@@ -4,12 +4,19 @@
 		<header>
 			<div class="tab">
 				<template v-for="(v, index) in tabList">
-					<span>{{v}}</span>
+					<span :class="{current: selectNum == index}">{{v}}</span>
 				</template>
 			</div>
 			<i class="icon icon_search"></i>
 		</header>
-		
+		<nav>
+			<span class="current">商品</span>
+			<span>股指期货</span>
+			<span>外汇</span>
+			<span>LME金属</span>
+			<span>债券期货</span>
+			<span>ETF</span>
+		</nav>
 		
 	</div>
 </template>
@@ -23,7 +30,10 @@
 		},
 		data(){
 			return{
+				selectNum: 1,
 				tabList: ['自选', '市场'],
+				selectTypeNum: 0,
+				TypeList: [],
 			}
 		},
 	}
@@ -38,8 +48,45 @@
 		width: $w;
 		height: 1rem;
 		background: $bg;
+		border-bottom: 0.01rem solid $black;
+		.tab{
+			width: 2rem;
+			height: 0.6rem;
+			margin: 0.2rem auto;
+			span{
+				display: inline-block;
+				float: left;
+				width: 1rem;
+				height: 0.6rem;
+				line-height: 0.6rem;
+				text-align: center;
+				background: $lightBlue;
+				&.current{
+					background: $black;
+				}
+			}
+		}
+		.icon_search{
+			width: 0.4rem;
+			height: 0.4rem;
+			background: url(../assets/images/search.png) no-repeat center center;
+			background-size: 100% 100%;
+			position: fixed;
+			top: 0.3rem;
+			right: 0.3rem;
+		}
 	}
-	.tab{
+	nav{
+		position: fixed;
+		top: 1rem;
+		left: 0;
+		width: $w;
+		height: 0.8rem;
+		line-height: 0.8rem;
+		border-bottom: 0.01rem solid $black;
 		
 	}
+	
+	
+	
 </style>
