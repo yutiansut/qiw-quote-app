40592abff8a950eda093ea636d9d73bb72246304
wuxuanfeng <527140631@qq.com>
2018-01-10 10:40:09 +0800

    111

--- src/page/quote/optionalManage.vue
+++ src/page/quote/optionalManage.vue
@@ -37,27 +37,53 @@
 							<span>10:30:21</span>
 						</div>
 						<div class="drag">
-							<i class="icon icon_"></i>
+							<i class="icon icon_drag"></i>
 						</div>
 					</li>
 					<li>
 						<!--<i class="icon icon_check"></i>-->
 						<i class="icon icon_checked"></i>
-						<div class="name">名称</div>
-						<div class="price">现价</div>
-						<div class="drag">拖动</div>
+						<div class="name">
+							<span>日经225</span>
+							<span>CNQ16</span>
+						</div>
+						<div class="price">
+							<span>72.5</span>
+							<span>10:30:21</span>
+						</div>
+						<div class="drag">
+							<i class="icon icon_drag"></i>
+						</div>
 					</li>
 					<li>
 						<i class="icon icon_check"></i>
 						<!--<i class="icon icon_checked"></i>-->
-						<div class="name">名称</div>
-						<div class="price">现价</div>
-						<div class="drag">拖动</div>
+						<div class="name">
+							<span>日经225</span>
+							<span>CNQ16</span>
+						</div>
+						<div class="price">
+							<span>72.5</span>
+							<span>10:30:21</span>
+						</div>
+						<div class="drag">
+							<i class="icon icon_drag"></i>
+						</div>
 					</li>
 				</ul>
 			</div>
 		</div>
-		
+		<div class="tools">
+			<div class="col">
+				<i class="icon icon_radio"></i>
+				<span>全选</span>
+			</div>
+			<div class="col">
+				<i class="icon icon_del"></i>
+				<span>删除</span>
+				<em>(1)</em>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -166,22 +192,77 @@
 				div{
 					float: left;
 					text-align: right;
-					&:nth-child(2){
+					&.name{
 						width: 3rem;
 						text-align: left;
 					}
-					&:nth-child(3){
+					&.price{
 						width: 1.5rem;
 					}
-					&:nth-child(4){
+					span{
+						display: block;
+						&:first-child{
+							font-size: $fs32;
+							color: $white;
+							margin: 0.15rem 0 0.1rem 0;
+						}
+					}
+					&.drag{
 						float: right;
 						width: 1rem;
+						.icon_drag{
+							float: right;
+							width: 0.24rem;
+							height: 0.24rem;
+							background: url(../../assets/images/drag.png) no-repeat center center;
+							background-size: 100% 100%;
+							margin: 0.36rem 0.15rem 0 0;
+						}
 					}
+					
 				}
 			}
 		}
 	}
-	
-	
-	
+	.tools{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 7.5rem;
+		height: 0.96rem;
+		border-top: 0.01rem solid $black;	
+		background: $titleBlue;
+		.col{
+			float: left;
+			width: 50%;
+			height: 0.96rem;
+			line-height: 0.96rem;
+			text-align: center;
+			border-left: 0.01rem solid $black;
+			&:first-child{
+				border: none;
+			}
+			.icon, span, em{
+				float: left;
+			}
+			em{
+				margin-left: 0.1rem;
+				color: $white;
+			}
+			.icon_radio{
+				width: 0.24rem;
+				height: 0.24rem;
+				background: $black;
+				border-radius: 0.12rem;
+				margin: 0.35rem 0.1rem 0 1.48rem;
+			}
+			.icon_del{
+				width: 0.24rem;
+				height: 0.24rem;
+				background: url(../../assets/images/delete.png) no-repeat center center;
+				background-size: 100% 100%;
+				margin: 0.35rem 0.1rem 0 1.24rem;
+			}
+		}	
+	}
 </style>
