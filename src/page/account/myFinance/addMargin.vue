248f38a8fdd4c5a67826e3a6caf1815254e71e50
chenbeisheng <864654012@qq.com>
2018-01-26 18:00:29 +0800

    222

--- src/page/account/myFinance/addMargin.vue
+++ src/page/account/myFinance/addMargin.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="addMargin">
 		<mt-header title="追加保证金" fixed style="background-color:#242933;font-size: 0.32rem;height: 1rem;border-bottom: 1px solid #12141a;line-height: 1rem;">
-		  	<router-link to="/account" slot="left">
+		  	<router-link to="/financeDetails" slot="left">
 		    	<i id="back"></i>
 		  	</router-link>
 		    <router-link to="/customerServices" slot="right">
@@ -10,17 +10,17 @@
 		</mt-header>
 		<div id="container">
 			<div class="ipt">
-				<input type="text" placeholder="追加金额 请输入充值金额"/>
+				<input type="text" placeholder="追加金额 请输入充值金额" v-model="addMoney"/>
 			</div>
 			<div class="info">
 				<ul>
-					<li>余额：<span>1080.00</span>元</li>
-					<li>换算汇率：<span>1美元=6.9人民币</span></li>
-					<li>换算美元：<span>0.00</span>美元</li>
+					<li>余额：<span>{{balance}}</span>元</li>
+					<li>换算汇率：<span>1美元={{cnyToUsdRate}}人民币</span></li>
+					<li>换算美元：<span>{{moneyUsd}}</span>美元</li>
 				</ul>
 			</div>
 			<div class="bt">
-				<btn className="orangelg" name="立即追加"></btn>
+				<btn className="orangelg" name="立即追加" @click.native="add"></btn>
 			</div>
 			<div class="warn">
 				<ul>
@@ -36,12 +36,130 @@
 
 <script>
 	import btn from "../../../components/btn.vue"
+	import pro from "../../../assets/js/common.js"
 	export default{
 		name:"addMargin",
 		components:{btn},
 		data(){
 			return{
-				
+				id:"",
+				balance:"",
+				//汇率
+				cnyToUsdRate:"",
+				addMoney:'',
+				moneyUsd:'',
+				cnyToSimulationRate:'',
+				addReg:/^\d+$/
+			}
+		},
+		methods:{
+			add:function(){
+				if(this.addMoney == ''){
+					this.$toast({message:"请输入追加保证金金额",duration: 1000});
+				}else if(this.addMoney<5){
+					this.$toast({message:"充值金额必须大于元",duration: 1000});
+				}else if(this.addReg.test(this.addMoney)==false){
+					this.$toast({message:"输入金额格式错误",duration: 1000});
+				}else{
+					var data = {
+						id:this.id,
+						money:this.addMoney*this.cnyToSimulationRate
+					}
+					var headers = {
+						token : this.userInfo.token,
+					secret : this.userInfo.secret
+					}
+					pro.fetch("post","/ futureManage/addMargin",data,headers).then((res)=>{
+						if(res.code == 1 && res.success == true){
+							this.$toast({message:"追加成功",duration: 1000});
+//							this.$router.push({path:"/"})
+						}
+					}).catch((err)=>{
+						var data = err.data;
+						if(data == undefined){
+							this.$toast({message:"网络不给力，请稍后再试",duration: 1000});
+						}else{
+							if(data.code == -9999){
+								this.$toast({message:"认证失败，请重新登录",duration: 1000});
+								this.$router.push({path:"/login"});
+							}
+							else{
+								this.$toast({message:data.message,duration: 1000});
+							}
+						}
+					})
+				}
+			},
+			getUserInfo:function(){
+				var headers = {
+					token : this.userInfo.token,
+					secret : this.userInfo.secret
+				}
+				pro.fetch("post","/account/getBasicMsg","",headers).then((res)=>{
+//					console.log("res==="+JSON.stringify(res));
+					if(res.code == 1 && res.success == true){
+						this.balance = res.data.balance;
+					
+					}
+				}).catch((err)=>{
+					var data = err.data;
+					if(data == undefined){
+						this.$toast({message:"网络不给力，请稍后再试",duration: 1000});
+					}else{
+						if(data.code == -9999){
+							this.$toast({message:"认证失败，请重新登录",duration: 1000});
+							this.$router.push({path:"/login"});
+						}
+						else{
+							this.$toast({message:data.message,duration: 1000});
+						}
+					}
+				})
+			},
+			//获取汇率
+			getCnyToUsdRate:function(){
+				pro.fetch("post","/others/getSysparam","","").then((res)=>{
+					console.log("res==="+JSON.stringify(res));
+					if(res.code == 1 && res.success == true){
+						this.cnyToUsdRate = 1/res.data.cnyToUsdRate;
+						this.cnyToSimulationRate = res.data.cnyToSimulationRate;
+						console.log("this.cnyToUsdRate==="+this.cnyToUsdRate)
+					}
+				}).catch((err)=>{
+					var data = err.data;
+					if(data == undefined){
+						this.$toast({message:"网络不给力，请稍后再试",duration: 1000});
+					}else{
+						if(data.code == -9999){
+							this.$toast({message:"认证失败，请重新登录",duration: 1000});
+							this.$router.push({path:"/login"});
+						}
+						else{
+							this.$toast({message:data.message,duration: 1000});
+						}
+					}
+				})
+			}
+		},
+		mounted:function(){
+			this.userInfo = localStorage.user ? JSON.parse(localStorage.user) : '';
+			this.id = this.$route.query.id;
+//			console.log(this.userInfo);
+			if(this.userInfo == ''){
+				console.log("1111")
+			}else{
+				this.getUserInfo();
+				this.getCnyToUsdRate();
+			}
+		},
+		activated: function(){
+			//获取平台账户登录信息
+			this.userInfo = localStorage.user ? JSON.parse(localStorage.user) : '';
+			this.id = this.$route.query.id;
+		},
+		watch:{
+			addMoney:function(a){
+				this.moneyUsd = a*(1/this.cnyToUsdRate);
 			}
 		}
 	}
