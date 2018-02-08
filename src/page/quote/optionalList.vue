ec864fbc5ed2869488ca90317739661e54b22073
wuxuanfeng <527140631@qq.com>
2018-02-08 17:18:13 +0800

    111

--- src/page/quote/optionalList.vue
+++ src/page/quote/optionalList.vue
@@ -241,7 +241,26 @@
 					if(res.data && res.data.length > 0){
 						this.$store.state.market.commodityOrder = res.data;
 						this.$parent.optionalList = res.data;
+						this.goodsList = [];
+						this.stockList = [];
+						this.foreignList = [];
+						this.metalList = [];
+						this.bondList = [];
+						this.etfList = [];
 						res.data.forEach((o, i) => {
+							if(o.commodityType == 1){
+								this.goodsList.push(o);
+							}else if(o.commodityType == 2){
+								this.stockList.push(o);
+							}else if(o.commodityType == 3){
+								this.foreignList.push(o);
+							}else if(o.commodityType == 4){
+								this.metalList.push(o);
+							}else if(o.commodityType == 5){
+								this.bondList.push(o);
+							}else if(o.commodityType == 6){
+								this.etfList.push(o);
+							}
 							this.quoteSocket.send('{"Method":"Subscribe","Parameters":{"ExchangeNo":"' + o.exchangeNo + '","CommodityNo":"' + o.commodityNo + '","ContractNo":"' + o.contractNo +'"}}');
 						});
 					}
