5d4f27589babc0406aa8cdac701040bc7db5571b
chenbeisheng <864654012@qq.com>
2018-02-28 09:32:51 +0800

    222

--- src/page/trade/tradeApply.vue
+++ src/page/trade/tradeApply.vue
@@ -232,7 +232,7 @@
 				}else{
 					//未领取过新手礼包
 					if(this.isPresentedgive == false){
-						MessageBox.alert("恭喜小主，您有1w的操盘体验金未领取，赶紧领取吧！","提示",{confirmButtonText:"去领取"}).then(action => {
+						MessageBox.alert("恭喜小主，您有10000模拟币未领取，赶紧领取吧！","提示",{confirmButtonText:"去领取"}).then(action => {
 //							console.log("999999999");
 							this.$router.push({path:"/newbieTask"});
 						});
